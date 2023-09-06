aeab024057cc749237a1274d57c39ecb05b66bc4
Szymon Brodawka <szymbrod@gmail.com>
2023-09-06 13:47:16 +0200

    Addin window event listener for debouncing resizing canvas elements function and creating sky, grass and player objects

--- scripts/canvas/debounceResizeCanvas.js
+++
@@ -1,5 +0,0 @@
-import { debounce } from "../utility/debounce.js";
-import { resizeCanvas } from "./resizeCanvas.js";
-
-const RESIZE_DELAY = 100;
-export const debounceResizeCanvas = debounce(resizeCanvas, RESIZE_DELAY);
--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,25 @@
 import { Rectangle } from "./Rectangle/Rectangle.js";
-import { debounceResizeCanvas } from "./canvas/debounceResizeCanvas.js";
-import { resizeCanvas } from "./canvas/resizeCanvas.js";
-import { handlePlayerMovement } from "./player/handlePlayerMovement.js";
+import { debounceResizeCanvas } from "./DOM/canvas/debounceResizeCanvas.js";
+import { resizeCanvas } from "./DOM/canvas/resizeCanvas.js";
+import { Player } from "./Player/Player.js";
+import { handlePlayerMovement } from "./Player/handlePlayerMovement.js";
+import { debounceresizeCanvasElements } from "./DOM/canvas/debounceresizeCanvasElements.js";
+
+resizeCanvas();
 
 const canvas = document.querySelector("#canvas");
-const context = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 
-const rectangle = new Rectangle(200, 100, 50, 100, "blue");
-rectangle.create(context);
+const WINDOW_HEIGHT = window.innerHeight;
+const WINDOW_WIDTH = window.innerWidth;
+const PLAYER_SIZE = WINDOW_WIDTH / 25;
+const PLAYER_X_SPAWN = WINDOW_WIDTH / 20;
+const PLAYER_Y_SPAWN = (WINDOW_HEIGHT - PLAYER_SIZE) / 2;
+const PLAYER_COLOR = "purple";
 
-window.addEventListener("load", () => resizeCanvas());
+const sky = new Rectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT / 2, "lightblue", ctx);
+const grass = new Rectangle(0, WINDOW_HEIGHT, WINDOW_WIDTH, -(WINDOW_HEIGHT / 2), "lightcyan", ctx);
+const player = new Player(PLAYER_X_SPAWN, PLAYER_Y_SPAWN, PLAYER_SIZE, PLAYER_SIZE, PLAYER_COLOR, ctx);
+
 window.addEventListener("resize", () => debounceResizeCanvas());
-window.addEventListener("keydown", (event) =>{
-    handlePlayerMovement(event, rectangle);
-});
+window.addEventListener("resize", () => debounceresizeCanvasElements(grass, sky, player));
