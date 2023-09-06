7346c149079fe7bd43ed2395dafd4b627e3e77b9
Szymon Brodawka <szymbrod@gmail.com>
2023-09-06 13:43:51 +0200

    Adding method scaling the objects of Rectangle class

--- scripts/Rectangle/Rectangle.js
+++ scripts/Rectangle/Rectangle.js
@@ -1,14 +1,25 @@
 export class Rectangle {
-    constructor(xPosition, yPosition, width, height, color) {
-        this.xPosition = xPosition;
-        this.yPosition = yPosition;
+    constructor(x, y, width, height, color, ctx) {
+        this.x = x;
+        this.y = y;
         this.width = width;
         this.height = height;
         this.color = color;
+        this.ctx = ctx;
+        this.initialize();
     }
 
-    create(context) {
-        context.fillStyle = this.color
-        context.fillRect(this.xPosition, this.yPosition, this.width, this.height);
+    initialize() {
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    scale(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
