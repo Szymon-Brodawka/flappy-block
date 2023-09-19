51dede4b3ca86c5894351cc19eb4c5b5430ddbc8
Szymon Brodawka <szymbrod@gmail.com>
2023-09-19 13:56:33 +0200

    Adding a speed property for Pipe scaling with window's width

--- scripts/classes/Pipe/Pipe.js
+++ scripts/classes/Pipe/Pipe.js
@@ -5,9 +5,10 @@ export class Pipe {
         this.randomNumber = Math.random() * (Math.random() * 10) + 2;
         this.height = Math.floor(window.innerHeight / this.randomNumber);
         this.width = playerSide * 1.5;
+        this.gap = playerSide * 2.5;
         this.ctx = ctx;
         this.color = color;
-        this.gap = playerSide * 4;
+        this.speed = window.innerWidth / 200;
         this.#initialize();
     }
     
@@ -26,17 +27,16 @@ export class Pipe {
     }
 
     scale(playerSide) {
-        this.x = window.innerWidth / 1.25;
         this.width = playerSide * 1.5;
         this.height = window.innerHeight / this.randomNumber;
-        this.gap = playerSide * 4;
+        this.gap = playerSide * 2.5;
         this.draw();
         this.#createBottomPipe();
     }
 
     moveLeft() {
-        if(this.x <= 0) return;
-        this.x -= 10;
+        if(this.x <= -this.width) return;
+        this.x -= this.speed;
         this.draw();
         this.#createBottomPipe();
     }
