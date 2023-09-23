d4eb9a64fab4279ca656e33762cf3a6a8ab44af7
Szymon Brodawka <szymbrod@gmail.com>
2023-09-23 13:17:18 +0200

    Adding checking for collision for bottom pipe

--- scripts/gameLogic/collisions/checkForPipeColision.js
+++ scripts/gameLogic/collisions/checkForPipeColision.js
@@ -1,20 +1,31 @@
+const rectIntersect = (x1, y1, w1, h1, x2, y2, w2, h2) => {
+    if (x2 > w1 + x1 || x1 > w2 + x2 || y2 > h1 + y1 || y1 > h2 + y2) {
+        return false;
+    }
+    return true;
+}
+
 export const checkForPipeColision = (player, pipes) => {
     let isColiding = false;
+    
     for(let i = 0; i < pipes.length; i++) {
         let pipe = pipes[i];
-
-        if(pipe.x < 0) {
-            return;
-        }
+        let bottomPipeHeight = window.innerHeight - pipe.height - pipe.gap;
+        let bottomPipeY = pipe.height + pipe.gap;
 
         if(
-            player.x > pipe.x ||
-            player.x > pipe.x && player.y < pipe.y + pipe.height
-            || player.x > pipe.x && player.y > pipe.y + pipe.height + pipe.gap
+            rectIntersect(
+                player.x, player.y, player.side, player.side,
+                pipe.x, pipe.y, pipe.width, pipe.height
+            )
+            || rectIntersect(
+                player.x, player.y, player.side, player.side,
+                pipe.x, bottomPipeY, pipe.width, bottomPipeHeight
+            )
         ) {
             return !isColiding;
         }
 
-        return isColiding;
+        return isColiding;;
     }
 }
